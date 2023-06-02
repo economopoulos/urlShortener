a25c4b3390284dfd2d041def8220e39ae3c51ea8
Angelos Economopoulos <aeconomopulos@gmail.com>
2023-06-02 13:26:03 +0300

    uploaded cto component

--- src/components/Cto/Cto.js
+++ src/components/Cto/Cto.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Cto.css'
 
 export default function Cto() {
   return (
-    <div>
-      
+    <div className='cto-container'>
+      <div className="main-content">
+        <section className='cto-section'>
+          <h2>Boost your links today</h2>
+          <button className='btn-type-a big'>get started</button>
+        </section>
+      </div>
     </div>
   )
 }
